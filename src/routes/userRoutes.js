f03470a93b896040cd8bf3cd92d931e659544638
Naldo de Andrade <106287645+francinaldodeandrade@users.noreply.github.com>
2023-10-02 15:18:56 -0300

    atualização da validação do token II

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,7 +8,7 @@ import auth from "../auth/validateToken.js";
 router.post("/create", userControllers.createUser)
 router.post("/login", authControllers.login)
 
-router.get("/getAll", auth.validateToken, userControllers.getAll)
+router.get("/getAll", userControllers.getAll)
 router.get("/getById/:id", auth.validateToken, userControllers.getById)
 router.put("/updateById/:id", auth.validateToken, userControllers.updateById)
 router.delete("/removeById/:id", auth.validateToken, userControllers.removeById)
